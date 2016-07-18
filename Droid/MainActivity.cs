5ce5a1ea1a530406160407dba0a292cb9f44d285
Jonathan Fraimorice <yonifra@gmail.com>
2016-07-19 00:09:36 +0300

    Auth0 working

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -2,24 +2,31 @@
 using Android.Widget;
 using Android.OS;
 using Auth0.SDK;
+using System;
+using Android.Support.Design.Widget;
+using Android.Views;
 
 namespace FiveMin.Core.Droid
 {
     [Activity (Label = "FiveMin", MainLauncher = true, Icon = "@mipmap/icon")]
-    public class MainActivity : Activity{
+    public class MainActivity : Activity
+    {
+        private Auth0Client _auth0;
+        private View _view;
 
-        private Auth0Client auth0;
         protected override void OnCreate (Bundle savedInstanceState)
         {
-            auth0 = new Auth0Client ("cryptocodes.auth0.com", "nYc5cfTehBlni7yCB4QpfgJQOu5876ce");
+            _auth0 = new Auth0Client ("cryptocodes.auth0.com", "nYc5cfTehBlni7yCB4QpfgJQOu5876ce");
             base.OnCreate (savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView (Resource.Layout.Main);
 
+            _view = FindViewById<LinearLayout> (Resource.Id.mainView);
+
             // Get our button from the layout resource,
             // and attach an event to it
-            Button button = FindViewById<Button> (Resource.Id.myButton);
+            Button button = FindViewById<Button> (Resource.Id.loginButton);
 
             button.Click += delegate {
                 ShowLogin ();
@@ -28,15 +35,34 @@ namespace FiveMin.Core.Droid
 
         private async void ShowLogin ()
         {
-            var user = await auth0.LoginAsync (this);
+            Auth0User user = null;
+            try
+            {
+                user = await _auth0.LoginAsync (this);
 
-            var usernameTv = FindViewById<TextView> (Resource.Id.usernameTextView);
+                if (user != null && user.Profile != null)
+                {
+                    var usernameTv = FindViewById<TextView> (Resource.Id.usernameTextView);
+                    var emailTv = FindViewById<TextView> (Resource.Id.emailTextView);
+                 //   var picTv = FindViewById<TextView> (Resource.Id.pictureTextView);
 
-            usernameTv.Text = user.Profile ["email"].ToString ();
+                    var email = user.Profile ["email"].ToString ();
+                    var name = user.Profile ["name"].ToString ();
+               //     var profile = user.Profile ["profile"]?.ToJson ();
+
+                    usernameTv.SetText (name, TextView.BufferType.Normal);
+                    emailTv.SetText (email, TextView.BufferType.Normal);
+
+                    Snackbar.Make (_view, string.Format ("Logged in as {0}", name), Snackbar.LengthLong).Show();
+                    //  picTv.Text = user.Profile
+                }
+            }
+            catch (Exception ex)
+            {
+                Snackbar.Make (_view, "Login failed", Snackbar.LengthLong).Show ();
+            }
         }
     }
-
-
 }
 
 
